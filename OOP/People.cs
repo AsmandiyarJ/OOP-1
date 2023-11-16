4db56ae82e7b86c85fe32f2b148530d4cd208469
Asmandiyar Jumaliev <jasmandiyar@gmail.com>
2023-11-16 20:57:06 +0600

    Добавьте файлы проекта.

---
+++ OOP/People.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class People
+    {
+        public int age;
+        public string surname = string.Empty;
+        public string name = string.Empty;
+
+
+        public void Setage(int age)
+        {
+            while (true)
+            {
+                if (age < 0 || age == 0)
+                {
+                    Console.WriteLine("Введите нормально возраст");
+                    age = int.Parse(Console.ReadLine());
+                    this.age = age;
+
+                }
+                else
+                {
+                    this.age = age;
+                    break;
+                }
+            }
+        }
+
+        public void SetSurname(string surname)
+        {
+            while (true)
+            {
+                if (surname == "")
+                {
+                    Console.WriteLine("Введите Фамилию");
+                    surname = Console.ReadLine();
+                    this.surname = surname;
+                }
+                else
+                {
+                    this.surname = surname;
+                    break;
+                }
+            }
+        }
+
+        public void Setname(string name)
+        {
+            while (true)
+            {
+                if (name == "")
+                {
+                    Console.WriteLine("Введите имя");
+                    name = Console.ReadLine();
+                    this.name = name;
+                }
+                else
+                {
+                    this.name = name;
+                    break;
+                }
+            }
+        }
+
+        public void Print(int age,string surname,string name)
+        {
+            Console.WriteLine("Ваш Возраст :" + age);
+            Console.WriteLine("Ваш Фамилия :" + surname);
+            Console.WriteLine("Ваш Имя :" + name);
+        }
+
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,18 @@
+﻿using OOP;
+People people = new People();
+Console.WriteLine("Введите свой возраст");
+people.age = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите свое Фамилия");
+people.surname = Console.ReadLine();
+Console.WriteLine("Введите свое имя");
+people.name = Console.ReadLine();
+
+
+people.Setage(people.age);
+people.SetSurname(people.surname);
+people.Setname(people.name);
+people.Print(people.age, people.surname,people.name);
+
+
+
+
